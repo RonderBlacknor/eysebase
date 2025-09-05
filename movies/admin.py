4e9588d841180952017631ce8fe51874d0bcc3fe
RonderBlacknor <klacenkovandrej49@gmail.com>
2025-09-05 18:42:15 +0300

    feat: Создали админ-панель и добавили модели

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Genre, Movie, Director
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'release_year', 'rating', 'is_published', 'director')
+    list_filter = ('is_published', 'genres', 'release_year')
+    search_fields = ('title', 'discription')
+@admin.register(Genre)
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Director)
+class MovieAdmin(admin.ModelAdmin):
+    pass
