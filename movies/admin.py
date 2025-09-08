5bfb7e563431c75488dae7e40241d95c984e0a99
RonderBlacknor <klacenkovandrej49@gmail.com>
2025-09-08 19:17:52 +0300

    feat: Настроили админ панель с помощью наборов атрибутов

--- movies/admin.py
+++ movies/admin.py
@@ -6,6 +6,29 @@ class MovieAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'release_year', 'rating', 'is_published', 'director')
     list_filter = ('is_published', 'genres', 'release_year')
     search_fields = ('title', 'discription')
+
+# не изменять данные 
+    readonly_fields = ('time_create', 'time_update')
+
+# Картечж будет содержатьб другие картеджы
+
+    fieldsets = (
+        # Картеджи состоящие из двух, Заголовок и словарей с настройками и ключ fields
+        ('Основная Информация', {
+           'fields': ('title', 'description', 'poster')
+        }), 
+        ('Данные о релизе', {
+           'fields': (('release_year', 'rating'),)
+        }), 
+        ('Связи', {
+           'fields': ('director', 'genres')
+        }), 
+        ('Статус', {
+           'classes':('collapse',),
+           'fields': ('is_published', 'time_create', 'time_update')
+        })
+    )
+
 @admin.register(Genre)
 class MovieAdmin(admin.ModelAdmin):
     pass
